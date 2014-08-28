d6b9b24ba13fe8f1db3f8ecb7f4bf802444f7904
ShawnVIP <gaoshuang@GaoShuangtekiMacBook-Pro.local>
2014-08-28 18:33:10 +0800

    upload res

--- res/uploadres.php
+++ res/uploadres.php
@@ -1,6 +1,12 @@
 <?php
 
 $mode=$_POST[mode];
+$dir=$_POST[radio];
+if($dir =="root"){
+	$dir="";
+}else{
+	$dir.="/";
+}
 $password=md5($_POST[password]);
 $outstr="";
 if($mode==1 & $password=="f9146ac536fabd9e06a112f4f8c28d66"){
@@ -12,7 +18,9 @@ if($mode==1 & $password=="f9146ac536fabd9e06a112f4f8c28d66"){
 	//echo $binfilename;
 	$fileext =substr($binfilename, strrpos($binfilename, '.') + 1);
 	$i=48;
-	while ( file_exists($binfilename)){
+	$filedir="../".$dir;
+	
+	while ( file_exists($filedir.$binfilename)){
 		$binfilename=$oldname ."_" . chr($i) . "." . $fileext;
 		$i++;
 		if($i>57){$i=97;}
@@ -20,11 +28,11 @@ if($mode==1 & $password=="f9146ac536fabd9e06a112f4f8c28d66"){
 	
 	//echo $binfilename;
 	if($binfilename != $finalname){
-		$outstr=" rename $finalname to $binfilename. <br>";
-		rename($finalname,$binfilename);
+		$outstr=" rename " .$filedir.$finalname ." to ". $filedir .$binfilename. ". <br>";
+		rename($filedir.$finalname,$filedir.$binfilename);
 	}
-	$outstr .= " upload $finalname sucessful. <br>";
-	move_uploaded_file($_FILES["binfile"]["tmp_name"], $finalname );
+	$outstr .= " upload ". $filedir .$finalname ." sucessful. <br>";
+	move_uploaded_file($_FILES["binfile"]["tmp_name"], $filedir.$finalname );
 }
 ?>
 
@@ -78,12 +86,25 @@ a:visited { text-decoration: none;color: green}
 
 </style>
 </head>
+<script>
+$(function(){
+	$('input:radio[value="<?php echo $_POST[radio]; ?>"]').attr("checked","checked"); 
+});
+</script>
 <body><p></p>
 <form action="uploadres.php" id="uploadform" method="post" enctype="multipart/form-data">
 <table width=800 align="center">
 <tr>
   <td class="right">pass:</td><td><input type="password" name="password" /></td></tr>
-<tr><tr>
+<tr>
+  <td class="right">direct:</td><td>
+  <input name="radio" type="radio" value="root" />root |  
+    <input name="radio" type="radio" value="res" />res |
+    <input type="radio" name="radio" value="js" />js |
+    <input type="radio" name="radio" value="cn" />cn | 
+    <input type="radio" name="radio" value="en" />en
+    </td></tr>
+<tr>
   <td class="right">file:</td><td><input type="file" name="binfile" /></td></tr>
 <tr><td class="right"></td><td><input type="hidden" name="mode" value=1 /><input type="submit" value="Send" /></td></tr>
 <tr><td></td><td><?php echo $outstr; ?></td></tr>
